2ef16f49fcabb0ee66705f78b42909337dd10157
Greatemmet <greatemmet@gmail.com>
2023-09-25 14:10:03 +0100

    Added nav semantics

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,10 +5,10 @@ import navLogo from '../../assets/images/toll-face.png'
 
 export default function Navbar() {
   return (
-    <div className="navbar">
+    <nav className="navbar">
       <img src={navLogo} alt="" className="navLogo" />
       <h1 className="navTitle">Meme Generator</h1>
       <h3 className="navProject">React Course - Project 3</h3>
-    </div>
+    </nav>
   );
 }
