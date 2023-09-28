9b7ef515f304a6420f4a7fbc33e9b2ba177ba62a
Greatemmet <greatemmet@gmail.com>
2023-09-28 13:02:09 +0100

    Recieved props to display meme url

--- src/components/Display/Display.jsx
+++ src/components/Display/Display.jsx
@@ -1,15 +1,12 @@
-import React from 'react'
-import './Display.scss'
+import React from "react";
+import "./Display.scss";
 
 export default function Display(props) {
-
- 
   return (
-    <div className='display'>
-      <img src={props.meme.url} alt="" className='memeImg'/>
+    <div className="display">
+      <img src={props.meme.url} alt="" className="memeImg" />
       <h1 className="topDisplay">topText</h1>
       <h1 className="bottomDisplay">bottomText</h1>
-      
     </div>
-  )
+  );
 }
