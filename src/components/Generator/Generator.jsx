2608915816c195041afcc6e40daf43dd676900a1
Greatemmet <greatemmet@gmail.com>
2023-10-03 11:30:19 +0100

    Added a text state to manage the top and bottom text, and a handleChange function to set the Text State when a change event happens

--- src/components/Generator/Generator.jsx
+++ src/components/Generator/Generator.jsx
@@ -8,28 +8,52 @@ export default function Generator() {
   const memes = memesData.data.memes;
 
   const [meme, setMeme] = React.useState("");
+  const [text, setText] = React.useState({
+    topText: "",
+    bottomText: "",
+  });
 
   function generateImg() {
     const randomNum = Math.floor(Math.random() * memes.length);
     setMeme(memes[randomNum]);
   }
 
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setText((prevState) => {
+      return {
+        ...prevState,
+        [name]: value,
+      };
+    });
+  }
+
   return (
     <div className="generator">
       <div className="inputField">
-        <input type="text" placeholder="Enter top text" className="topText" />
+        <input
+          type="text"
+          placeholder="Enter top text"
+          className="topText"
+          onChange={handleChange}
+          value={text.topText}
+          name="topText"
+        />
         <input
           type="text"
           placeholder="Enter bottom text"
           className="bottomText"
+          onChange={handleChange}
+          value={text.bottomText}
+          name="bottomText"
         />
       </div>
-      
+
       <p className="generateBtn" onClick={generateImg}>
         Get a new meme image 🖼
       </p>
 
-      <Display meme={meme} />
+      <Display meme={meme} text={text} />
     </div>
   );
 }
