1cb757453ecb89126b004f168e4ebe4077dc1fe4
Greatemmet <greatemmet@gmail.com>
2023-10-03 11:34:40 +0100

    Recieved props and displayed the top and bottom text

--- src/components/Display/Display.jsx
+++ src/components/Display/Display.jsx
@@ -5,8 +5,10 @@ export default function Display(props) {
   return (
     <div className="display">
       <img src={props.meme.url} alt="" className="memeImg" />
-      {props.meme.url && <h1 className="topDisplay">topText</h1>}
-      {props.meme.url && <h1 className="bottomDisplay">bottomText</h1>}
+      {props.meme.url && <h1 className="topDisplay">{props.text.topText}</h1>}
+      {props.meme.url && (
+        <h1 className="bottomDisplay">{props.text.bottomText}</h1>
+      )}
     </div>
   );
 }
